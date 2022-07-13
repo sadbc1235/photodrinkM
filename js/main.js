7bdd37e275bb1d0ce900c19221927f490221a69c
sadbc1235 <sadbc1235@naver.com>
2022-07-13 16:29:49 +0900

    add fade out effect

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ const boothLen = boothBoxs.length;
 // booth names effect
 const boothNames = document.querySelector(".boothNames");
 const itemNames = boothNames.querySelectorAll(".itemName");
+// booth name, conceptBtn, explain fade out effect
+const boothNameBoxs = document.querySelectorAll(".boothNameBox");
+const conceptBtns = document.querySelectorAll(".conceptBtn");
+const boothExplains = document.querySelectorAll(".boothExplain");
 
 boothBtn.addEventListener("click", () => {
 
@@ -16,27 +20,43 @@ boothBtn.addEventListener("click", () => {
 
   for(let i=0; i<boothLen; i++) {
     boothBoxs[i].style.display = "block";
+    boothNameBoxs[i].style.opacity = 0;
+    conceptBtns[i].style.opacity = 0;
+    boothExplains[i].style.opacity = 0;
   }
 
   currentIndex !== boothLen-1
     ? (nextIndex = currentIndex+1)
     : (nextIndex = 0);
 
-    currentBooth.classList.add("left");
-    currentBooth.classList.remove("active");
-    currentName.classList.remove("active");
-    boothBoxs[nextIndex].classList.add("right");
-
     setTimeout(() => {
-      boothBoxs[nextIndex].classList.add("active");
-      itemNames[nextIndex].classList.add("active");
-      boothBoxs[nextIndex].classList.remove("right");
-      currentBooth.classList.remove("left");
-      for(let i=0; i<boothLen; i++) {
-        if (i !== nextIndex) {
-          boothBoxs[i].style.display = "none";
+      currentBooth.classList.add("left");
+      currentBooth.classList.remove("active");
+      currentName.classList.remove("active");
+      boothBoxs[nextIndex].classList.add("right");
+
+      setTimeout(() => {
+        boothBoxs[nextIndex].classList.add("active");
+        itemNames[nextIndex].classList.add("active");
+        boothBoxs[nextIndex].classList.remove("right");
+        currentBooth.classList.remove("left");
+
+        for(let i=0; i<boothLen; i++) {
+          if (i !== nextIndex) {
+            boothBoxs[i].style.display = "none";
+          }
         }
-      }
-    }, 500)
+
+        setTimeout(() => {
+          for(let i=0; i<boothLen; i++) {
+            boothNameBoxs[i].style.opacity = 1;
+            conceptBtns[i].style.opacity = 1;
+            boothExplains[i].style.opacity = 1;
+          }
+        }, 600)
+
+      }, 500)
+
+    }, 500);
 
 })
