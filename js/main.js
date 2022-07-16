764ba841143b42c51dd5497d0a710673391d0b1a
sadbc1235 <sadbc1235@naver.com>
2022-07-17 02:22:48 +0900

    making booth page to concept page effect

--- js/main.js
+++ js/main.js
@@ -109,3 +109,101 @@ for(let i=0; i<itemNames.length; i++) {
   
   })
 }
+
+// concept sample item click event
+const conceptBoxWrapp = document.querySelector(".conceptBoxWrapp");
+const conceptBoxs = conceptBoxWrapp.querySelectorAll(".conceptBox");
+
+for(let i=0; i<conceptBoxs.length; i++) {
+  const itemSamples = conceptBoxs[i].querySelectorAll(".itemSample");
+  const imgSamples = conceptBoxs[i].querySelectorAll("img");
+
+  for (let i=0; i<itemSamples.length; i++) {
+    itemSamples[i].addEventListener("click", () => {
+      for(let i=0; i<imgSamples.length; i++) {
+        imgSamples[i].classList.remove("active");
+      }
+      imgSamples[i].classList.add("active");
+    })
+  }
+}
+
+// switch concepts effect
+const upBtn = document.querySelector(".upArrow");
+let upText = upBtn.querySelector(".arrowName");
+const downBtn = document.querySelector(".downArrow");
+let downText = downBtn.querySelector(".arrowName");
+
+upBtn.addEventListener("click", () => {
+  let currentConcept = conceptBoxWrapp.querySelector(".conceptBox.active");
+  let currentIndexConcept = parseInt(currentConcept.getAttribute("data-index"));
+  let nextIndexConcept = null;
+
+  let currentSampleBox = currentConcept.querySelector(".sampleBox");
+
+
+  currentIndexConcept !== conceptBoxs.length-1
+    ? (nextIndexConcept = currentIndexConcept+1)
+    : (nextIndexConcept = 0);
+
+  let nextSampleBox = conceptBoxs[nextIndexConcept].querySelector(".sampleBox");
+
+  currentSampleBox.classList.remove("active");
+
+  setTimeout(() => {
+    currentConcept.classList.add("up");
+    currentConcept.classList.remove("active");
+    conceptBoxs[nextIndexConcept].classList.add("down");
+
+    setTimeout(() => {
+      conceptBoxs[nextIndexConcept].classList.add("active");
+      conceptBoxs[nextIndexConcept].classList.remove("down");
+      upText.querySelector("h3").innerText = conceptBoxs[(nextIndexConcept === conceptBoxs.length-1) ? 0 : nextIndexConcept + 1].querySelector("h2").innerText;
+      downText.querySelector("h3").innerText = conceptBoxs[currentIndexConcept].querySelector("h2").innerText;
+      currentConcept.classList.remove("up")
+
+      setTimeout(() => {
+        nextSampleBox.classList.add("active");
+      }, 480)
+
+    }, 400);
+
+  }, 300)
+})
+
+downBtn.addEventListener("click", () => {
+  let currentConcept = conceptBoxWrapp.querySelector(".conceptBox.active");
+  let currentIndexConcept = parseInt(currentConcept.getAttribute("data-index"));
+  let nextIndexConcept = null;
+
+  let currentSampleBox = currentConcept.querySelector(".sampleBox");
+
+
+  currentIndexConcept !== 0
+    ? (nextIndexConcept = currentIndexConcept-1)
+    : (nextIndexConcept = conceptBoxs.length-1);
+
+  let nextSampleBox = conceptBoxs[nextIndexConcept].querySelector(".sampleBox");
+
+  currentSampleBox.classList.remove("active");
+
+  setTimeout(() => {
+    currentConcept.classList.add("down");
+    currentConcept.classList.remove("active");
+    conceptBoxs[nextIndexConcept].classList.add("up");
+
+    setTimeout(() => {
+      conceptBoxs[nextIndexConcept].classList.add("active");
+      conceptBoxs[nextIndexConcept].classList.remove("up");
+      downText.querySelector("h3").innerText = conceptBoxs[(nextIndexConcept === 0) ? conceptBoxs.length-1 : nextIndexConcept - 1].querySelector("h2").innerText;
+      upText.querySelector("h3").innerText = conceptBoxs[currentIndexConcept].querySelector("h2").innerText;
+      currentConcept.classList.remove("down")
+
+      setTimeout(() => {
+        nextSampleBox.classList.add("active");
+      }, 480)
+
+    }, 400);
+
+  }, 300)
+})
