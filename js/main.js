ce840f4f3ce18c30cc7b735358de1726dac52e47
sadbc1235 <sadbc1235@naver.com>
2022-07-17 15:32:06 +0900

    complete concept page

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ boothBtn.addEventListener("click", () => {
 
       }, 500)
 
-    }, 500);
+    }, 350);
 
 })
 
@@ -105,7 +105,7 @@ for(let i=0; i<itemNames.length; i++) {
   
         }, 500)
   
-      }, 500);
+      }, 350);
   
   })
 }
@@ -117,13 +117,17 @@ const conceptBoxs = conceptBoxWrapp.querySelectorAll(".conceptBox");
 for(let i=0; i<conceptBoxs.length; i++) {
   const itemSamples = conceptBoxs[i].querySelectorAll(".itemSample");
   const imgSamples = conceptBoxs[i].querySelectorAll("img");
+  const itemNames = conceptBoxs[i].querySelectorAll(".sampleName");
 
   for (let i=0; i<itemSamples.length; i++) {
     itemSamples[i].addEventListener("click", () => {
+      
       for(let i=0; i<imgSamples.length; i++) {
         imgSamples[i].classList.remove("active");
+        itemNames[i].classList.remove("active");
       }
       imgSamples[i].classList.add("active");
+      itemNames[i].classList.add("active");
     })
   }
 }
@@ -133,6 +137,7 @@ const upBtn = document.querySelector(".upArrow");
 let upText = upBtn.querySelector(".arrowName");
 const downBtn = document.querySelector(".downArrow");
 let downText = downBtn.querySelector(".arrowName");
+let conceptBoxLength = 3;
 
 upBtn.addEventListener("click", () => {
   let currentConcept = conceptBoxWrapp.querySelector(".conceptBox.active");
@@ -142,7 +147,7 @@ upBtn.addEventListener("click", () => {
   let currentSampleBox = currentConcept.querySelector(".sampleBox");
 
 
-  currentIndexConcept !== conceptBoxs.length-1
+  currentIndexConcept !== conceptBoxLength-1
     ? (nextIndexConcept = currentIndexConcept+1)
     : (nextIndexConcept = 0);
 
@@ -158,7 +163,7 @@ upBtn.addEventListener("click", () => {
     setTimeout(() => {
       conceptBoxs[nextIndexConcept].classList.add("active");
       conceptBoxs[nextIndexConcept].classList.remove("down");
-      upText.querySelector("h3").innerText = conceptBoxs[(nextIndexConcept === conceptBoxs.length-1) ? 0 : nextIndexConcept + 1].querySelector("h2").innerText;
+      upText.querySelector("h3").innerText = conceptBoxs[(nextIndexConcept === conceptBoxLength-1) ? 0 : nextIndexConcept + 1].querySelector("h2").innerText;
       downText.querySelector("h3").innerText = conceptBoxs[currentIndexConcept].querySelector("h2").innerText;
       currentConcept.classList.remove("up")
 
@@ -181,7 +186,7 @@ downBtn.addEventListener("click", () => {
 
   currentIndexConcept !== 0
     ? (nextIndexConcept = currentIndexConcept-1)
-    : (nextIndexConcept = conceptBoxs.length-1);
+    : (nextIndexConcept = conceptBoxLength-1);
 
   let nextSampleBox = conceptBoxs[nextIndexConcept].querySelector(".sampleBox");
 
@@ -195,7 +200,7 @@ downBtn.addEventListener("click", () => {
     setTimeout(() => {
       conceptBoxs[nextIndexConcept].classList.add("active");
       conceptBoxs[nextIndexConcept].classList.remove("up");
-      downText.querySelector("h3").innerText = conceptBoxs[(nextIndexConcept === 0) ? conceptBoxs.length-1 : nextIndexConcept - 1].querySelector("h2").innerText;
+      downText.querySelector("h3").innerText = conceptBoxs[(nextIndexConcept === 0) ? conceptBoxLength-1 : nextIndexConcept - 1].querySelector("h2").innerText;
       upText.querySelector("h3").innerText = conceptBoxs[currentIndexConcept].querySelector("h2").innerText;
       currentConcept.classList.remove("down")
 
@@ -207,3 +212,76 @@ downBtn.addEventListener("click", () => {
 
   }, 300)
 })
+
+// Move to concept page from booth page
+const boothBtnBox = document.querySelector(".boothBtnBox");
+const footer = document.querySelector("footer");
+
+for(let i=0; i<conceptBtns.length; i++) {
+  conceptBtns[i].addEventListener("click", () => {
+
+    if(i === 0 || i === 1) {
+      conceptBoxLength = 3;
+    } else if (i === 2) {
+      conceptBoxLength = 4;
+    }
+
+
+    boothBoxWrapp.classList.add("active");
+    boothBtnBox.classList.add("active");
+    boothBtnBox.style.display = "none";
+    footer.classList.add("active");
+
+    setTimeout(() => {
+      conceptBoxWrapp.classList.remove("active");
+      conceptBoxs[0].classList.add("active");
+      conceptBoxs[0].querySelectorAll("img")[0].classList.add("active");
+      conceptBoxs[0].querySelectorAll(".sampleName")[0].classList.add("active");
+
+      setTimeout(() => {
+        conceptBoxs[0].querySelector(".sampleBox").classList.add("active");
+      }, 300)
+
+    }, 200)
+    
+  })
+}
+
+// Move to booth page from concept page
+const backBtns = document.querySelectorAll(".backBtn");
+
+for(let i=0; i<backBtns.length; i++) {
+  backBtns[i].addEventListener("click", () => {
+
+    for(let i=0; i<conceptBoxs.length; i++) {
+      conceptBoxs[i].classList.remove("active");
+      conceptBoxs[i].querySelector(".sampleBox").classList.remove("active");
+    }
+
+    setTimeout(() => {
+      boothBtnBox.style.display = "flex";
+      conceptBoxWrapp.classList.add("active");
+
+      setTimeout(() => {
+        boothBoxWrapp.classList.remove("active");
+        boothBtnBox.classList.remove("active");
+        footer.classList.remove("active");
+
+        for(let i=0; i<conceptBoxs.length; i++) {
+          for(let j=0; j<conceptBoxs[i].querySelectorAll("img").length; j++) {
+            conceptBoxs[i].querySelectorAll("img")[j].classList.remove("active");
+            conceptBoxs[i].querySelectorAll(".sampleName")[j].classList.remove("active");  
+          }
+          conceptBoxs[i].querySelectorAll("img")[0].classList.add("active");
+          conceptBoxs[i].querySelectorAll(".sampleName")[0].classList.add("active");  
+        }
+
+      }, 300)
+
+    }, 200)
+    
+
+    
+  })
+}
+
